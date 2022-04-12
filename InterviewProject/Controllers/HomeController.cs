5b33da814c4ad232bcf51c6034151524003a15cd
duybao1210 <duybao12102000@gmail.com>
2022-04-12 22:25:23 +0700

    first commit

---
+++ InterviewProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using InterviewProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private VNR_InternShipEntities db = new VNR_InternShipEntities();
+        public ActionResult Index()
+        {          
+            return View();
+        }
+        public ActionResult ListCourse()
+        {
+            List<KhoaHoc> lstCourse = db.KhoaHocs.ToList();
+            return View(lstCourse);
+        }
+
+        public ActionResult DetailCourse(int? id)
+        {
+            Session["nameCourse"] = null;
+            if (id != null)
+            {
+                var Course = db.KhoaHocs.Find(id);
+                Session["nameCourse"] = Course.TenKhoaHoc;
+            }
+            List<MonHoc> lstSubject = db.MonHocs.Where(p => p.KhoaHocID == id).ToList();
+            Session["idCourse"] = null;          
+            return PartialView("DetailCourse",lstSubject);
+        }
+    }
+}
